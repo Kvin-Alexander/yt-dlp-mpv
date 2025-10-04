67b3126171dceb41f050cdb21180247b5259999b
Alexander <bearyuer@outlook.com>
2025-10-04 19:53:34 +0800

    2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 #include<cstdlib>
 #include<string>
-#include<windows.h>
-#include<vector>
 
 /*
 1.用户输入网址
@@ -13,26 +11,32 @@
 
 
 int main() {
-	//std::vector<std::string> videoFiles;
-	/*std::cout << "请输入完整视频名称(例子.mp4):";
-	std::string name;
-	std::cin >> name;*/
-
 	//1.用户输入网址
 	std::cout << "请输入视频网址:";
 	std::string url;
 	std::cin >> url;
 	//2.yt-dlp解析网址，下载视频
-	std::string downloadCommand = "yt-dlp -o \"downloaded_video.%(ext)s\" " + url;
+	std::string downloadCommand = "part.yt-dlp\\yt-dlp -o \"downloaded_video.%(ext)s\" " + url + " >nul 2>nul";
 	system(downloadCommand.c_str());
 	//假设下载的视频文件名为downloaded_video.mp4
 	std::string name = "downloaded_video.mp4";
 	//3.运行视频
-	std::string command = "mpv \"" + name + "\"";
-	system(command.c_str());
+	//fs::current_path(originalPath);
+	std::string command3 = "part.mpv\\mpv \"" + name + "\""; 
+	system(command3.c_str());
 	//4.删除视频
-	std::string deleteCommand = "del \"" + name + "\"";
-	system(deleteCommand.c_str());
+	std::cout << "是否删除视频文件？（y/n） ";
+	char choice[4];
+	std::cin >> choice;
+	if ('y' == choice[0]) {
+		std::cout << "正在删除文件..." << std::endl;
+		std::string deleteCommand = "del \"" + name + "\"";
+		system(deleteCommand.c_str());
+	}
+	else {
+		std::cout << "文件保存在当前目录下，文件名为downloaded_video.mp4" << std::endl;
+	}
+
 
 
 
