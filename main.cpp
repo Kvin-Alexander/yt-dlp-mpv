c51c7fcb820d7cb72f80c859f6a536a837d4e4d7
Kvin-Alexander <bearyuer@outlook.com>
2025-10-05 21:58:57 +0800

    05

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<cstdlib>
 #include<string>
+#include<chrono>
+#include<ctime>
 
 /*
 1.用户输入网址
@@ -10,18 +12,26 @@
 */
 
 
+//检测时间
+
+
 int main() {
+	auto now = std::chrono::system_clock::now();
+	std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+	std::tm local_time;
+	localtime_s(&local_time, &now_c);
+	char timeStr[32];
+	strftime(timeStr, sizeof(timeStr), "%Y-%m-%d-%H%M%S", &local_time);
+	std::string name = std::string(timeStr) + ".mp4";
 	//1.用户输入网址
 	std::cout << "请输入视频网址:";
 	std::string url;
 	std::cin >> url;
 	//2.yt-dlp解析网址，下载视频
-	std::string downloadCommand = "part.yt-dlp\\yt-dlp -o \"downloaded_video.%(ext)s\" " + url + " >nul 2>nul";
+	std::string downloadCommand = "part.yt-dlp\\yt-dlp -o \"" + name + "\" " + url;// +" >nul 2>nul";
 	system(downloadCommand.c_str());
 	//假设下载的视频文件名为downloaded_video.mp4
-	std::string name = "downloaded_video.mp4";
 	//3.运行视频
-	//fs::current_path(originalPath);
 	std::string command3 = "part.mpv\\mpv \"" + name + "\""; 
 	system(command3.c_str());
 	//4.删除视频
@@ -33,9 +43,6 @@ int main() {
 		std::string deleteCommand = "del \"" + name + "\"";
 		system(deleteCommand.c_str());
 	}
-	else {
-		std::cout << "文件保存在当前目录下，文件名为downloaded_video.mp4" << std::endl;
-	}
 
 
 
@@ -44,3 +51,4 @@ int main() {
 
 	return 0;
 }
+
